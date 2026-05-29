228e015c9f74d680acb9b2af73b3195fc1b6d73c
AnamikaSingh15 <anamikasingh53417@gmail.com>
2026-05-29 12:15:24 +0545

    Main update

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Library Management System");
+        System.out.println("Library Management System - Main");
         System.out.println("Book Issue Feature Added");
     }
 }
